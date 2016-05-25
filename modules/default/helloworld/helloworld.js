abfed48e4f02ead29ddf6321deb2b5e6e097a6da
raspberrypy <py@raspberry.com>
2016-05-25 11:15:19 +0200

    Riconfigurato il MagicMirror

--- modules/default/helloworld/helloworld.js
+++ modules/default/helloworld/helloworld.js
@@ -7,6 +7,7 @@
  * MIT Licensed.
  */
 
+
 Module.register("helloworld",{
 
 	// Default module config.
@@ -14,10 +15,46 @@ Module.register("helloworld",{
 		text: "Hello World!"
 	},
 
+
+    
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
+	    // var jsFileLocation = $('script[src*=example]').attr('src'); 
+	    // wrapper.innerHTML = jsFileLocation;
+	    var scriptEls = document.getElementsByTagName( 'script' );
+	    var thisScriptEl = scriptEls[scriptEls.length - 1];
+	    var scriptPath = thisScriptEl.src;
+	    var scriptFolder = scriptPath.substr(0, scriptPath.lastIndexOf( '/' )+1 );
+	   // wrapper.innerHTML = scriptFolder;
+
+	    // this.file('prova.gg')
+	    
+	    console.log("__dirname = %s", window.location.pathname);
+
+	    // // lets use a namespace to avoid globals.
+	    // var myApp = {
+	    // 	// still need to set this when DOM/body is ready
+	    // 	rootContext: document.body.getAttribute("data-root"),
+	    // 	getContext: function( src ) {
+	    // 	    return this.rootContext + src;
+	    // 	}
+	    // }
+	    
+	    var img = document.createElement("img");
+    // img.src = myApp.getContext( 'foto.jpg' );  //'foto.jpg';
+	    img.src = scriptFolder+'foto.png';
+
+
+    img.width = 500;
+	    img.height = 300;
+	    img.style.position = "absolute";
+        img.style.top = '100px';
+        img.style.left = '500px';
+	    img.alt = 'Google Logo';
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+	    return wrapper;
 	}
 });
